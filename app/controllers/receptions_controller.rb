24e4c140736b320bc72b5a9319481d3e4fc61615
Vladislav <vladislav.belyaev.93@gmail.com>
2017-03-10 18:23:12 +0200

    Update return book

--- app/controllers/receptions_controller.rb
+++ app/controllers/receptions_controller.rb
@@ -2,7 +2,7 @@ class ReceptionsController < ApplicationController
   before_filter :set_book, only: [:take_book, :return_book]
   def new_take_book
     @books = Book.available.order('name asc').pluck(:name, :id)
-    @users = User.order('first_name asc').pluck(:email, :id)
+    @users = User.regular.order('first_name asc').pluck(:email, :id)
   end
 
   def new_return_book
@@ -10,6 +10,7 @@ class ReceptionsController < ApplicationController
   end
 
   def take_book
+    redirect_to :back , alert: 'Choose user' and return if params[:user_id].blank?
     @book.update(user_id: params[:user_id], status: :unavailable)
     redirect_to new_take_book_receptions_path, notice: 'Book has been successfully taken'
   end
@@ -23,6 +24,7 @@ class ReceptionsController < ApplicationController
   private
 
   def set_book
+    redirect_to :back , alert: 'Choose book' and return if params[:book_id].blank?
     @book = Book.find(params[:book_id])
   end
 end
