deabf128877a856abaec6d2992229b37f2f214f4
Vladislav <vladislav.belyaev.93@gmail.com>
2017-03-10 17:03:53 +0200

    Update authorization.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,11 @@ class SessionsController < ApplicationController
   end
 
   def create
+    redirect_to login_path, alert: 'Invalid user' and return if regular_user?
     if login(params[:email], params[:password])
       redirect_to root_path, notice: 'Logged in successfully'
     else
-      redirect_to sign_in_path, alert: 'Email or password invalid'
+      redirect_to login_path, alert: 'Email or password invalid'
     end
   end
 
@@ -16,4 +17,11 @@ class SessionsController < ApplicationController
     redirect_to root_path, notice: 'Logged out successfully'
   end
 
+  private
+
+  def regular_user?
+    user = User.find_by(email: params[:email])
+    return unless user
+    user.regular?
+  end
 end
